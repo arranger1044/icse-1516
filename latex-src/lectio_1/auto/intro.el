0b5269a9bb0d5c2485509db0c362ae924ae53ab3
valerio <arranger1044@aim.com>
2016-04-20 09:13:47 +0200

    Adding lec 1 sources

---
+++ latex-src/lectio_1/auto/intro.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "intro"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor={usenames,dvipsnames,svgnames}" "compress")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontspec" "no-math")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "../colors"
+    "beamer"
+    "beamer10"
+    "booktabs"
+    "dcolumn"
+    "colortbl"
+    "hyperref"
+    "ifxetex"
+    "amsmath"
+    "biblatex"
+    "fontspec")
+   (TeX-add-symbols
+    "small")))
+
