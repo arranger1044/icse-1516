aa36a8f40b796b8a3d7ac786d6e2bf3ce1029884
valerio <arranger1044@aim.com>
2016-05-23 19:49:00 +0200

    Adding sources for latest lecture

---
+++ latex-src/lectio_4/auto/lectio_4.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "lectio_4"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor={usenames,dvipsnames,svgnames}" "compress")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontspec" "no-math")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "../clips-listings"
+    "../colors"
+    "beamer"
+    "beamer10"
+    "booktabs"
+    "dcolumn"
+    "colortbl"
+    "ifxetex"
+    "amsmath"
+    "biblatex"
+    "fontspec")
+   (TeX-add-symbols
+    "small")))
+
